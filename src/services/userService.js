d00fb4bfbcf6615df6c3550b1746b56ce8edcbcd
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-25 04:48:31 +0000

    mail from info@mahalaxmifinancial.com sent

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,19 +3,24 @@ const nodemailer = require("nodemailer");
 exports.Mail = async (userData) => {
     try {
       const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        service: "gmail",
+        host: "smtp.ionos.com",
+        service: "ionos",
         port: 587,
-        secure: true,
+        secureConnection: false,
         auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASSWORD
+          user: "info@mahalaxmifinancial.com",
+          pass: "neha@654321"
         },
+        tls: {
+          ciphers:'SSLv3'
+      }
       });
-      const text = "Name: " + userData.name + "\nCity: " + userData.city + "\nPhone: " + userData.phone + "\nFeedback: " + userData.feedback;
+      const text = "Hi,\n\nA new request has been received. Details of the person are:\n\nName: " 
+      + userData.name + "\nCity: " + userData.city + "\nPhone: " + userData.phone +"\nEmail: "
+      + userData.email + "\nMessage: " + userData.feedback+"\n\nThanks,\nMahalaxmi Financial Services";
       await transporter.sendMail({
-        from: process.env.EMAIL,
-        to: userData.email,
+        from: "info@mahalaxmifinancial.com",
+        to: "financialmahalaxmi@gmail.com",
         subject: userData.subject,
         text: text
       });
